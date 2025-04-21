92d5c7513d6a0f0c85267eab58dd6bafbe14197f
RAMESHKUMAR K <ramesh0505rk@gmail.com>
2025-04-21 10:47:53 +0530

    Updated cors policy.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
 // Add services to the container.
 
 builder.Services.AddControllers();
