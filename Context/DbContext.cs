51cda696d87aa255f2ed9914938ae5ce65c22023
RAMESHKUMAR <t476797@deluxe.com>
2025-04-21 11:44:00 +0530

    Implemented DbContext.

---
+++ Context/DbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace CraftServer.Context
+{
+    public class DbContext
+    {
+        private readonly string _connectionString;
+        public DbContext(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("CraftConn");
+        }
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
