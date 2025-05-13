1107f8adf923d4864494630a95940f6c3a9dfefd
RAMESHKUMAR KRISHNASAMY <t476797@deluxe.com>
2025-05-13 13:40:51 +0530

    Implemented ResetPasswordController.

---
+++ Controllers/ResetPasswordController.cs
@@ -0,0 +1,44 @@
+﻿using CraftServer.Context;
+using CraftServer.Models;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CraftServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResetPasswordController : ControllerBase
+    {
+        DbContext _context;
+        public ResetPasswordController(DbContext context)
+        {
+            _context = context;
+        }
+        [HttpPost("requestOtp")]
+        public async Task<IActionResult> RequestOtp([FromBody] string email)
+        {
+            try
+            {
+                var query = "SELECT * FROM Users WHERE UserEmail = @UserEmail";
+                using var connection = _context.CreateConnection();
+                var user = await connection.QueryFirstOrDefaultAsync<User>(query, new { UserEmail = email });
+
+                if (user == null)
+                {
+                    return BadRequest(new { message = "A user with this email doesn't exists" });
+                }
+
+                var otp = new Random().Next(1000, 9999).ToString("D4");
+
+                return Ok(new { message = "OTP sent successfully", otp = otp });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = "An error occurred while requesting OTP.", error = ex.Message });
+            }
+        }
+    }
+}
