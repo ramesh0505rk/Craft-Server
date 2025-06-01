0332837990ee64d0fc82e0fe84c7553383f607d4
RAMESHKUMAR K <ramesh0505rk@gmail.com>
2025-06-01 23:06:40 +0530

    Updated the SendOtpEmail method.

--- Controllers/ResetPasswordController.cs
+++ Controllers/ResetPasswordController.cs
@@ -4,6 +4,7 @@ using Dapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace CraftServer.Controllers
@@ -13,15 +14,18 @@ namespace CraftServer.Controllers
     public class ResetPasswordController : ControllerBase
     {
         private DbContext _context;
-        public ResetPasswordController(DbContext context)
+        private IConfiguration _config;
+        public ResetPasswordController(DbContext context, IConfiguration configuration)
         {
             _context = context;
+            _config = configuration;
         }
         [HttpPost("requestOtp")]
-        public async Task<IActionResult> RequestOtp([FromBody] string email)
+        public async Task<IActionResult> RequestOtp(UserEmail userEmail)
         {
             try
             {
+                string email = userEmail.Email;
                 var query = "SELECT * FROM Users WHERE UserEmail = @UserEmail";
                 using var connection = _context.CreateConnection();
                 var user = await connection.QueryFirstOrDefaultAsync<User>(query, new { UserEmail = email });
@@ -51,7 +55,28 @@ namespace CraftServer.Controllers
 
         public async Task SendOtpEmail(string email, string otp)
         {
+            var emailSettings = _config.GetSection("EmailSettings");
+            var message = new MailMessage
+            {
+                From = new MailAddress(emailSettings["SenderEmail"], "Craft App"),
+                Subject = "Your OTP for password reset",
+                Body = @$"Your OTP for password reset is: <strong>{otp}</strong>
+                       <br/>This OTP is valid for 10 minutes",
+                IsBodyHtml = true
+            };
+            message.To.Add(email);
 
+            using var smtpClient = new SmtpClient(emailSettings["SmtpServer"], int.Parse(emailSettings["SmtpPort"]))
+            {
+                Credentials = new System.Net.NetworkCredential(emailSettings["SenderEmail"], emailSettings["SenderAppPassword"]),
+                EnableSsl = true
+            };
+
+            await smtpClient.SendMailAsync(message);
         }
+    }
+    public class UserEmail
+    {
+        public string Email { get; set; } = "";
     }
 }
